f06b2acee2b8c423abd280ab20b0a717292f0350
rajkumar <rajkumarpandiyaraj@gmail.com>
2020-10-20 19:41:22 -0700

    Fibbonacci series using python

---
+++ fibb.py
@@ -0,0 +1,10 @@
+numOfSeries = int(input('Enter Number of series you want : '))
+z = 0
+i = 1
+print(z)
+print(i)
+for j in range(0, numOfSeries - 2) :
+    result = z + i
+    print(result)
+    z = i
+    i = result
